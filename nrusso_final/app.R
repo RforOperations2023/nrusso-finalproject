f9268e42be41f05c4c0a2d476aa36c0572183e11
Noahrusso <101607643+Noahrusso@users.noreply.github.com>
2023-03-03 23:20:52 -0500

    leaflet map adjustments
    data table

--- nrusso_final/app.R
+++ nrusso_final/app.R
@@ -14,12 +14,11 @@ library(sf)
 library(geojsonsf)
 
 df <- read_csv('IncHousing.csv')
-
 #reading in gejson us map for base leaflet
 usmap  <- geojson_sf('https://rstudio.github.io/leaflet/json/us-states.geojson')
-
 #joining csv to spatial data
-left_join(usmap, df, by = 'name') -> mapData
+bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
+pal <- colorBin("YlOrRd", domain = mapData$total_count, bins = bins)
 
 # list_df <- list(usmap,df)
 # mapData <- list_df %>% reduce(full_join, by='name')
@@ -28,9 +27,6 @@ left_join(usmap, df, by = 'name') -> mapData
 #another way to load data I didn't use
 #https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}{r}.png?access_token={accessToken}
 
-#defining bins and palettew for 
-bins <- c(0, 10, 20, 50, 100, 200, 500, 1000, Inf)
-pal <- colorBin("YlOrRd", domain = mapData$density, bins = bins)
 
 
 # Define UI 
@@ -44,54 +40,76 @@ ui <- navbarPage("Inclusionary Housing Policies in the US",
                             # Map Panel
                             mainPanel(
                               # Using Shiny JS
-                              shinyjs::useShinyjs(),
+                              # shinyjs::useShinyjs(),
                               # Style the background and change the page
-                              tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
-                                         body {background-color: #D4EFDF;}"),
+                              # tags$style(type = "text/css", ".leaflet {height: calc(100vh - 90px) !important;}
+                              #            body {background-color: #D4EFDF;}"),
                               # Map Page
                               leafletOutput("map")
                             )
                           )
-                 )#,
-                 # # Data Table Pannel
-                 # tabPanel("Data",
-                 #          fluidPage(
-                 #            wellPanel(DT::dataTableOutput("table"))
-                 #          )
-                 # )
+                 ),
+                 # Data Table Panel
+                 tabPanel("Data",
+                          fluidPage(
+                            wellPanel(DT::dataTableOutput("table"))
+                          )
+                 )
 )
 #info for this comes form https://rstudio.github.io/leaflet/choropleths.html
-# Define server logic required to draw a histogram
+#Define server logic required to draw a histogram
 server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
-    leaflet(mapData) %>%
+    leaflet(total_counts()) %>%
       setView(-96, 37.8, 4) %>%
-      # addProviderTiles("MapBox", options = providerTileOptions(
-      #   id = "mapbox.light",
-      #   accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
       addPolygons(
-        fillColor = "red",
-        weight = 2,
-        # opacity = 1,
-        color = "white",
+        fillColor = ~pal(total_count),
+        weight = 1,
+        opacity = 1,
+        color = "black",
         dashArray = "3",
         fillOpacity = 0.7,
         stroke = TRUE,
         highlightOptions = highlightOptions(
           weight = 5,
-          color = "#666",
+          color = "#663",
           dashArray = "",
           fillOpacity = 0.7,
           bringToFront = TRUE),
-        popup = ~paste0("<b>", name, "</b>"), group = "mapData", layerId = ~State, fill = FALSE,
+        popup = ~paste0("<b>", name, "</b>"),
+        group = "total_counts", layerId = ~name, fill = TRUE,
         labelOptions = labelOptions(
           style = list("font-weight" = "normal", padding = "3px 8px"),
           textsize = "15px",
           direction = "auto"))
     
-    
   })
+  
+  output$table <- DT::renderDataTable(
+    # if(input$show_data){
+      DT::datatable(data = df[,c( "ProgramName", "City", "State",
+                                      "County", "Program Applicable Areas",
+                                  "Minimum Set-aside")], 
+                    options = list(pageLength = 50), 
+                    rownames = FALSE))
+    # })
+  
+
+  total_counts <- function() {
+    emptydf <- data.frame()
+    agg_tbl <- df %>%
+      group_by(name) %>%
+      summarise(total_count = n())
+    left_join(usmap, agg_tbl, by = 'name') -> mapData
+    mapData[is.na(mapData)] <- 0
+    emptydf <- rbind(emptydf, mapData)
+    return(emptydf)
+  }
+
 }
 
 # Run the application 
